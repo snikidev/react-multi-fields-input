3884826aadbec47c493584cf6da68a5a00f3d878
nkakuev_cg <nikita.kakuev@capgemini.com>
2019-04-26 16:06:10 +0100

    new setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+const path = require('path');
+const webpack = require('webpack');
+const lintFormatter = require('eslint-formatter-friendly');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
 	entry: './src/index.tsx',
 	output: {
@@ -9,6 +14,12 @@ module.exports = {
 	},
 
 	devtool: 'source-map',
+
+	stats: 'errors-only',
+
+	node: {
+		fs: 'empty'
+	},
 
 	resolve: {
 		modules: [ path.resolve(__dirname, './src'), 'node_modules' ],
@@ -29,10 +40,24 @@ module.exports = {
 					failOnError: false
 				}
 			},
+			{
+				test: /\.(eot|svg|ttf|woff|woff2)$/,
+				loader: 'file-loader?name=app/fonts/[name].[ext]'
+			},
+			{
+				test: /\.(png|jpg|gif)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {}
+					}
+				]
+			},
 			{ test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
 			{ enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
 		]
 	},
+	plugins: [ new webpack.HotModuleReplacementPlugin(), new webpack.NamedModulesPlugin(), new UglifyJsPlugin() ],
 
 	externals: {
 		react: {
@@ -53,86 +78,3 @@ module.exports = {
 		}
 	}
 };
-
-// const path = require('path');
-// const webpack = require('webpack');
-// const lintFormatter = require('eslint-formatter-friendly');
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
-// module.exports = {
-// 	entry: './src/index.js',
-// 	output: {
-// 		filename: 'index.js',
-// 		path: path.resolve(__dirname, 'dist'),
-// 		publicPath: '/dist',
-// 		library: '',
-// 		libraryTarget: 'commonjs2'
-// 	},
-// 	resolve: {
-// 		modules: [ path.resolve(__dirname, './src'), 'node_modules' ],
-// 		extensions: [ '.js', '.jsx', '.json' ]
-// 	},
-
-// 	externals: {
-// 		react: {
-// 			commonjs: 'react',
-// 			commonjs2: 'react',
-// 			amd: 'React',
-// 			root: 'React'
-// 		},
-// 		'react-dom': {
-// 			commonjs: 'react-dom',
-// 			commonjs2: 'react-dom',
-// 			amd: 'ReactDOM',
-// 			root: 'ReactDOM'
-// 		},
-// 		'styled-components': {
-// 			commonjs: 'styled-components',
-// 			commonjs2: 'styled-components'
-// 		}
-// 	},
-
-// 	stats: 'errors-only',
-
-// 	node: {
-// 		fs: 'empty'
-// 	},
-
-// module: {
-// 	rules: [
-// 		{
-// 			enforce: 'pre',
-// 			test: /\.js?$/,
-// 			loader: 'eslint-loader',
-// 			exclude: /node_modules/,
-// 			options: {
-// 				configFile: './.eslintrc.json',
-// 				formatter: lintFormatter,
-// 				failOnWarning: false,
-// 				failOnError: false
-// 			}
-// 		},
-// 		{
-// 			test: /\.(eot|svg|ttf|woff|woff2)$/,
-// 			loader: 'file-loader?name=app/fonts/[name].[ext]'
-// 		},
-// 		{
-// 			test: /\.js?$/,
-// 			loader: 'babel-loader',
-// 			exclude: /node_modules/
-// 		},
-// 		{
-// 			test: /\.(png|jpg|gif)$/,
-// 			use: [
-// 				{
-// 					loader: 'file-loader',
-// 					options: {}
-// 				}
-// 			]
-// 		}
-// 	]
-// },
-// 	devtool: 'inline-source-map',
-
-// 	plugins: [ new webpack.HotModuleReplacementPlugin(), new webpack.NamedModulesPlugin(), new UglifyJsPlugin() ]
-// };

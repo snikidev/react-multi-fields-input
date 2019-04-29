b9158abb5688e87651b8938bb1d465d38c098ca8
Nikita K <n.kakuev@gmail.com>
2019-04-29 20:09:35 +0100

    styleguidist

--- src/components/views.tsx
+++ src/components/views.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import styled, { css } from 'styled-components';
+import * as styled from 'styled-components';
+import { css } from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
